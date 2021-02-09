f7cb85f0a6736bb1b68f79319dbcf657c4329849
Fabian Schmauder <fabian.schmauder@gmail.com>
2021-02-09 16:42:22 +0100

    Init student list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import StudentCard from "./components/StudentCard";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const students =[
+        {
+            id: "1",
+            name: "Frank",
+            uni: "cologne"
+        },
+        {
+            id: "2",
+            name: "Jochen",
+            uni: "Hamburg"
+        },
+        {
+            id: "3",
+            name: "Jochen 3",
+            uni: "Hamburg"
+        }
+    ];
+
+    return (
+        <div>
+            {students.map(student => <StudentCard key={student.id}
+                                                  title={"Name: "+student.name}
+                                                  description={"Uni: "+ student.uni}/>)}
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/StudentCard.js
@@ -0,0 +1,10 @@
+
+function StudentCard({title, description}) {
+
+    return <article>
+        <h3>{title}</h3>
+        <p>{description}</p>
+    </article>
+}
+
+export default StudentCard;
