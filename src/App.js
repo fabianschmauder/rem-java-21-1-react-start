665d89f43b911ee5d48b23b65c92ee7b9300563f
Fabian Schmauder <fabian.schmauder@gmail.com>
2021-02-10 11:42:32 +0100

    Init react state hook

--- src/App.js
+++ src/App.js
@@ -1,30 +1,37 @@
 import './App.css';
 import StudentCard from "./components/StudentCard";
+import {useState} from "react";
 
 function App() {
-    const students =[
-        {
-            id: "1",
-            name: "Frank",
-            uni: "cologne"
-        },
-        {
-            id: "2",
-            name: "Jochen",
-            uni: "Hamburg"
-        },
-        {
-            id: "3",
-            name: "Jochen 3",
-            uni: "Hamburg"
-        }
-    ];
+    const [students, setStudents] = useState([]);
 
     return (
         <div>
             {students.map(student => <StudentCard key={student.id}
-                                                  title={"Name: "+student.name}
-                                                  description={"Uni: "+ student.uni}/>)}
+                                                  title={"Name: " + student.name}
+                                                  description={"Uni: " + student.uni}/>)}
+
+            <button onClick={() =>
+                setStudents([
+                        {
+                            id: "1",
+                            name: "Frank",
+                            uni: "cologne"
+                        },
+                        {
+                            id: "2",
+                            name: "Jochen",
+                            uni: "Hamburg"
+                        },
+                        {
+                            id: "3",
+                            name: "Jochen 3",
+                            uni: "Hamburg"
+                        }
+                    ]
+                )}>
+                Load Students
+            </button>
         </div>
     );
 }
--- src/components/StudentCard.js
+++ src/components/StudentCard.js
@@ -1,9 +1,14 @@
+import {useState} from "react";
 
 function StudentCard({title, description}) {
-
+    const [clicked, setClicked] = useState(0);
     return <article>
         <h3>{title}</h3>
         <p>{description}</p>
+        <p>{clicked}</p>
+        <button onClick={() => {
+             setClicked(clicked + 1)
+        }}> Click me</button>
     </article>
 }
 
